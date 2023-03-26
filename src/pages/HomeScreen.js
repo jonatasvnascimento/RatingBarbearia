9e71862ec633bc84de9ca4a47269845aadd5a1b4
Jonatas Nascimento <jonatasv.nascimento@gmail.com>
2023-03-26 13:17:19 -0300

    dev: ajuste componente foto

--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -1,10 +1,13 @@
 import React, { useEffect } from 'react';
-import { View, SafeAreaView, Text, StyleSheet, Platform, ScrollView  } from 'react-native';
+import { View, SafeAreaView, Text, StyleSheet, Platform, ScrollView, Dimensions  } from 'react-native';
 import { DatabaseConnection } from '../database/database-connection';
 import { Card, Rating } from '@rneui/themed';
 import Ratings from './components/Ratings';
 
 const db = DatabaseConnection.getConnection();
+
+const { width, height } = Dimensions.get('window');
+const size = Math.min(width, height) * 0.1;
 
 const HomeScreen = ({ navigation }) => {
   useEffect(() => {
@@ -26,15 +29,75 @@ const HomeScreen = ({ navigation }) => {
     });
   }, []);
 
+  const Header = () => {
+    return (
+      <View style={styles.header}>
+        <Text>header</Text>
+      </View>
+    )
+  }
+
+  const Boxes = (foto) => {
+    const items = Array.from({ length: 10 }, (v, i) => i);
+  
+    return (
+      <View style={styles.boxContainer}>
+        {items.map((item) => (
+          <View key={item} style={styles.box}>
+            <View style={styles.inner}>
+              <Text>Foto</Text>
+            </View>
+          </View>
+        ))}
+      </View>
+    )
+  }
+
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <View style={{ flex: 1, backgroundColor: 'white' }}>
-        <Text>
-          <Ratings />
-        </Text>
-      </View>
+      <ScrollView>
+        <View style={{ flex: 1, backgroundColor: 'white' }}>
+          <Boxes></Boxes>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  boxContainer: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'red',
+    padding: 0,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+  },
+  box: {
+    width: size * 3,
+    height: size * 5,
+    backgroundColor: 'green',
+    padding: 0,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: '10%',
+    marginVertical: '5%',
+    borderRadius: size / 2,
+  },
+  inner: {
+    width: size * 3,
+    height: size * 3,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: size * 10,
+  },
+});
 
 export default HomeScreen;
